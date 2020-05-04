1488acd7a8251950dfed4f952249d3061550fb67
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-05-04 11:34:20 +0300

    Add new functions
    * hh-scp-clear-vendor
    * hh-scp-kernel
    * hh-update-all-android
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- henv.sh
+++ henv.sh
@@ -8,8 +8,9 @@
 #   HH_OUT_PRODUCT_IMAGE_COPY - directory to which result product image should be copied
 
 #export HH_ENABLED=1
-export HH_VERSION=0.2
-export HH_PATH=~/bin/hhelpers/henv.sh
+export HH_VERSION=0.3
+export HH_PRODUCT_WORKDIR=/mnt/hdd3/media/prod-devel/current
+export HH_PRODUCT_CONFIG=devel
 export HH_TMP_
 
 ################################################################
@@ -32,6 +33,11 @@ function hh()
   echo "HH_OUT_PRODUCT_IMAGE_COPY"=${HH_OUT_PRODUCT_IMAGE_COPY}
 }
 
+function _get_board_ip()
+{
+  export BOARD_IP=`systemctl -n 1 status isc-dhcp-server | tail -n1 | awk ' {print $8} '`
+}
+
 ################################################################
 # hh-aenv - will init Android env in current session, using
 #           HH_ANDROID_ENV, which point to Android env script
@@ -46,7 +52,9 @@ function hh-aenv()
 ################################################################
 function _connect_sync()
 {
-  adb connect `systemctl -n 1 status isc-dhcp-server | tail -n1 | awk ' {print $8} '`
+  _get_board_ip
+  adb connect ${BOARD_IP}":5555"
+  #adb connect `systemctl -n 1 status isc-dhcp-server | tail -n1 | awk ' {print $8} '`
   adb wait-for-device
 }
 
@@ -162,10 +170,7 @@ function hh-update()
   popd
 }
 
-function _get_board_ip()
-{
-  export BOARD_IP=`systemctl -n 1 status isc-dhcp-server | tail -n1 | awk ' {print $8} '`
-}
+
 
 # 3301965824 is a vendor offset
 # I.e. you need to check vendor MiB offset in mk_sdcard_image.sh
@@ -177,6 +182,53 @@ function hh-scp-vendor()
   ssh root@${BOARD_IP} "mkdir -p /home/root/vendor/ && ls -l && /sbin/losetup -o 3301965824 -f /dev/mmcblk0p3 && mount /dev/loop0 /home/root/vendor/"
   scp -r ${ANDROID_PRODUCT_OUT}/vendor root@${BOARD_IP}:/home/root/
   ssh root@${BOARD_IP} "sync && umount /dev/loop0 && /sbin/losetup -d /dev/loop0"
+}
+
+function hh-scp-clear-vendor()
+{
+  _get_board_ip
+  ssh root@${BOARD_IP} "mkdir -p /home/root/vendor/ && ls -l && /sbin/losetup -o 3301965824 -f /dev/mmcblk0p3 && mount /dev/loop0 /home/root/vendor/ && rm -rf /home/root/vendor/*"
+  scp -r ${ANDROID_PRODUCT_OUT}/vendor root@${BOARD_IP}:/home/root/
+  ssh root@${BOARD_IP} "sync && umount /dev/loop0 && /sbin/losetup -d /dev/loop0"
+}
+
+
+function hh-scp-kernel()
+{
+  KERNEL_PATH=${ANDROID_PRODUCT_OUT}/obj/KERNEL_OBJ/arch/arm64/boot/Image
+  TMP_IFS=/tmp/ifs
+
+  mkdir -p ${TMP_IFS}
+
+  _get_board_ip
+
+  ssh root@${BOARD_IP} "mkdir -p /home/root/ifs/ && mount /dev/mmcblk0p1 /home/root/ifs/"
+  scp root@${BOARD_IP}:/home/root/ifs/boot/uInitramfs ${TMP_IFS}/uInitramfs
+
+
+  pushd ${TMP_IFS}
+  uirfs.sh unpack uInitramfs ./u
+  cp ${KERNEL_PATH} ./u/xt/doma/Image
+  uirfs.sh pack uInitramfs ./u
+  popd
+
+  scp ${TMP_IFS}/uInitramfs root@${BOARD_IP}:/home/root/ifs/boot/uInitramfs
+  ssh root@${BOARD_IP} "sync && umount /dev/mmcblk0p1"
+
+}
+
+function hh-update-all-android()
+{
+  _get_board_ip
+  //hh-scp-kernel
+  pushd ${HH_PRODUCT_WORKDIR}
+  cp ${ANDROID_PRODUCT_OUT}/*.img ${HH_PRODUCT_WORKDIR}/domu-image-android/images/qemux86-64/
+  export TMP_IMG=./tmp.img
+  ./mk_sdcard_image.sh -p . -d ${TMP_IMG} -c ${HH_PRODUCT_CONFIG}
+  local loop_dev=`sudo losetup --find --partscan --show ${TMP_IMG}`
+  sudo dd if=${loop_dev}p3 bs=1M | ssh root@${BOARD_IP} 'dd of=/dev/mmcblk0p3 bs=1M status=progress && sync'
+  sudo losetup -d ${loop_dev}
+  popd
 }
 
 ################################################################
